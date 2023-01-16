5e34ba31171bb68930dce2d9a5bcf39bc98b9253
emirgonul <emirgonul@live.com>
2023-01-16 00:02:25 -0500

    re-org

---
+++ python/programs/state_guess_game/main.py
@@ -0,0 +1,58 @@
+#1. convert the guess to Title case
+#2. check if the guess is among the 50 states
+#3. write correct guesses onto the map
+#4. use a loop to allow the user to keep guessing
+#5. record the correct guesses in a list
+#6. keep track of the score
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+#add image to screen
+image = "blank_states_img.gif"
+turtle.addshape(image)
+turtle.shape(image)
+
+
+data = pandas.read_csv("50_states.csv")
+#convert data to list
+all_states = data.state.to_list()
+#keep track of guesses
+guessed_states = []
+
+#prompt until 50 states all are guessed
+while len(guessed_states) < 50:
+  answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", 
+                                  prompt="What's another state's name?").title()
+  #exit to kill game 
+  if answer_state == "Exit":
+    ##list comprehension
+    ##missing_states = [new_item for state in all_states if state not in guessed_states]
+    #new list of missing states on exit
+    missing_states = []
+    #for loop to go through all states
+    #if a state is not in guessed_states, it's a missed state
+    for state in all_states:
+      if state not in guessed_states:
+        #list of missed states
+        missing_states.append(state)
+    #construct data frame of missing states
+    new_data = pandas.DataFrame(missing_states)
+    #write to file states that were missed by the user
+    new_data.to_csv("states_to_learn.csv")
+
+    break
+  #if the answer is one of the states in the csv
+  #if correct, write name to the x, y coordinates
+  if answer_state in all_states:
+    #add to list when guess state is correct
+    guessed_states.append(answer_state)
+    #construct turtle
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    #retrieve the row where answer is equal to the list
+    state_data = data[data.state == answer_state]
+    t.goto(int(state_data.x), int(state_data.y))
+    t.write(answer_state)
