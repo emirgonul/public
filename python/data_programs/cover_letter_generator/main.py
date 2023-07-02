b26c3dddf5d2258aebbb96bad66768fa9dd2d0b2
Emir <emir@Cerens-Air.fios-router.home>
2023-07-01 21:58:31 -0400

    adding cover_letter generator folder path smarts

--- python/data_programs/cover_letter_generator/main.py
+++ python/data_programs/cover_letter_generator/main.py
@@ -1,18 +1,30 @@
-#pip install docxtpl
+import os
 from docxtpl import DocxTemplate
 
+#setup directory and variable info
+PATH = '/Users/emir/gitrepo/public/python/data_programs/cover_letter_generator/'
 company_name = input("Enter name of the Company : ")
 position_name = input("Enter name of the Position: ")
 
+#setup variables to be replaced
 context = { 
            'company_name' : company_name, 
            'position_name' : position_name
            }
 
-# Open our master template
-doc = DocxTemplate("/Users/emir/gitrepo/public/python/data_programs/cover_letter_generator/Emir_Gonul-CoverLetter.docx")
-print(doc)
-# Load them up
+#open cover letter
+doc = DocxTemplate(f"{PATH}Emir_Gonul-CoverLetter.docx")
+#Load up data
 doc.render(context)
+
+#check if a folder for company exists
+new_path = f'{PATH}letters/{company_name}'
+isExist = os.path.exists(new_path)
+#if folder doesnt exist create it 
+if not isExist:
+   # Create a new directory for the company
+   os.makedirs(new_path)
+   print(f"{company_name} directory is created!")
+
 # Save the file with personalized filename
-doc.save('/Users/emir/gitrepo/public/python/data_programs/cover_letter_generator/test.docx')
+doc.save(f'{PATH}letters/{company_name}/Emir_Gonul_'+company_name+'_'+position_name+'.docx')
