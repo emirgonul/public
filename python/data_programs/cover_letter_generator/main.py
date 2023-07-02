ce86d07b668d090fbba73d01f66f20967e0c22c0
Emir <emir@Cerens-Air.fios-router.home>
2023-07-02 18:10:20 -0400

    feature to select full/short name & record entry to csv for records

--- python/data_programs/cover_letter_generator/main.py
+++ python/data_programs/cover_letter_generator/main.py
@@ -1,5 +1,8 @@
 import os
+import pandas as pd
+from datetime import date
 from docxtpl import DocxTemplate
+#TODO: create functions
 
 #setup directory and variable info
 PATH = '/Users/emir/gitrepo/public/python/data_programs/cover_letter_generator/'
@@ -12,8 +15,15 @@ context = {
            'position_name' : position_name
            }
 
+#setup which name & file
+first_name = input('legal or short name?\n l for legal, s for short\n:').lower()
+if first_name == 'l':
+   name = 'Emirhan_Gonul'
+else:
+   name = "Emir_Gonul"
+
 #open cover letter
-doc = DocxTemplate(f"{PATH}Emir_Gonul-CoverLetter.docx")
+doc = DocxTemplate(f"{PATH}{name}-CoverLetter.docx")
 #Load up data
 doc.render(context)
 
@@ -27,9 +37,23 @@ if not isExist:
    print(f"{company_name} directory is created!")
 
 # Save the file with personalized filename
-doc.save(f'{PATH}letters/{company_name}/Emir_Gonul_'+company_name+'_'+position_name+'.docx')
+doc.save(f'{PATH}letters/{company_name}/{name}_'+company_name+'_'+position_name+'.docx')
 
 #verify file and print results
-success = os.path.exists(f'{PATH}letters/{company_name}/Emir_Gonul_'+company_name+'_'+position_name+'.docx')
+success = os.path.exists(f'{PATH}letters/{company_name}/{name}_'+company_name+'_'+position_name+'.docx')
 if success:
-   print(f'{company_name}/Emir_Gonul_'+company_name+'_'+position_name+'.docx created!')
+   print(f'{company_name}/{name}_'+company_name+'_'+position_name+'.docx created!')
+
+#create dict for csv file
+today = str(date.today())
+data = {
+   'Date': [today],
+   'Name': [company_name],
+   'Position': [position_name],
+   'Email': [name]   
+}
+#create data frame, #record entry to csv
+df = pd.DataFrame(data)
+df.to_excel('/Users/emir/Desktop/app_list.csv', mode='a', index=False, header=False) 
+# print message
+print("Data appended successfully.")
