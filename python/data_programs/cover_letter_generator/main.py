0ab972aeb6dee8fe6b3bf43b16515931f4173345
Emir <emir@Cerens-Air.fios-router.home>
2023-07-06 22:56:20 -0400

    created functions to improve functionality

--- python/data_programs/cover_letter_generator/main.py
+++ python/data_programs/cover_letter_generator/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from turtle import Screen
 from datetime import date
 from docxtpl import DocxTemplate
-#TODO: create functions
+#TODO: upgrade to class based program
 
 #setup prompt screen
 screen = Screen()
@@ -13,7 +13,6 @@ screen.setup(100, 50)
 PATH = '/Users/emir/gitrepo/public/python/data_programs/cover_letter_generator/'
 company_name = screen.textinput(title="Company Name", prompt="Enter name of the company: ").title()
 position_name = screen.textinput(title="Position Name", prompt="Enter name of the position: ").title()
-
 #setup variables to be replaced
 context = { 
            'company_name' : company_name, 
@@ -21,44 +20,54 @@ context = {
            }
 
 #setup which name & file
-first_name = screen.textinput(title="legal or short name?", prompt="Enter l for legal, s for short")
-if first_name == 'l':
-   name = 'Emirhan_Gonul'
-else:
-   name = "Emir_Gonul"
+def name_set():
+   first_name = screen.textinput(title="legal or short name?", prompt="Enter l for legal, s for short")
+   if first_name == 'l':
+      return 'Emirhan_Gonul'
+   else:
+      return "Emir_Gonul"
 
-#open cover letter
-doc = DocxTemplate(f"{PATH}{name}-CoverLetter.docx")
-#Load up data
-doc.render(context)
-
-#check if a folder for company exists
-new_path = f'{PATH}letters/{company_name}'
-isExist = os.path.exists(new_path)
-#if folder doesnt exist create it 
-if not isExist:
-   # Create a new directory for the company
-   os.makedirs(new_path)
-   print(f"{company_name} directory is created!")
-
-# Save the file with personalized filename
-doc.save(f'{PATH}letters/{company_name}/{name}_'+company_name+'_'+position_name+'.docx')
+def cover_doc(name):
+   #open cover letter
+   doc = DocxTemplate(f"{PATH}{name}-CoverLetter.docx")
+   #Load up data
+   doc.render(context)
+   #check if a folder for company exists
+   new_path = f'{PATH}letters/{company_name}'
+   isExist = os.path.exists(new_path)
+   #if folder doesnt exist create it 
+   if not isExist:
+      # Create a new directory for the company
+      os.makedirs(new_path)
+      print(f"{company_name} directory is created!")
+   #save the file with personalized filename
+   doc.save(f'{PATH}letters/{company_name}/{name}_'+company_name+'_'+position_name+'.docx')
 
 #verify file and print results
-success = os.path.exists(f'{PATH}letters/{company_name}/{name}_'+company_name+'_'+position_name+'.docx')
-if success:
-   print(f'{company_name}/{name}_'+company_name+'_'+position_name+'.docx created!')
+def success(name):
+   success = os.path.exists(f'{PATH}letters/{company_name}/{name}_'+company_name+'_'+position_name+'.docx')
+   if success:
+      print(f'{company_name}/{name}_'+company_name+'_'+position_name+'.docx created!')
 
-#create dict for csv file
-today = str(date.today())
-data = {
-   'Date': [today],
-   'Name': [company_name],
-   'Position': [position_name],
-   'Email': [name]   
-}
-#create data frame, #record entry to csv
-df = pd.DataFrame(data)
-df.to_csv(f'{PATH}/app_list.csv', mode='a', index=False, header=False) 
-# print message
-print("Data appended successfully.")
+def df(name):
+   #create dict for csv file
+   today = str(date.today())
+   data = {
+      'Date': [today],
+      'Name': [company_name],
+      'Position': [position_name],
+      'Email': [name]   
+   }
+   #create data frame, #record entry to csv
+   df = pd.DataFrame(data)
+   df.to_csv(f'{PATH}/app_list.csv', mode='a', index=False, header=False) 
+   #print message
+   print("Data appended successfully.")
+
+#main function to call other functions
+def main(name):
+   cover_doc(name)
+   success(name)
+   df(name)
+
+main(name = name_set()) 
